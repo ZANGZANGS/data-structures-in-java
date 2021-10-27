e93eef909f0aeef44438bdf03848e2efe1425944
ZANGZANGS 49123158+ZANGZANGS@users.noreply.github.com <kimjangh94@gmail.com>
2021-10-27 17:55:26 +0900

    add 메서드

--- src/data/structure/tree/Tree.java
+++ src/data/structure/tree/Tree.java
@@ -19,4 +19,37 @@ public class Tree<E> {
 
     }
 
+
+    //add function
+    public void add(E obj, Node<E> node){
+
+        if(((Comparable<E>)obj).compareTo(node.data)>0){
+            //go to right
+            if(null == node.right) {
+                node.right = new Node<E>(obj);
+                return;
+            }
+
+            add(obj, node.right);
+            return;
+        }else{
+            //go to left
+            if(null == node.left) {
+                node.left = new Node<E>(obj);
+                return;
+            }
+            add(obj, node.right);
+            return;
+        }
+
+    }
+
+    public  void add(E obj){
+        if(null == root){
+            root = new Node<E>(obj);
+        }else{
+            add(obj, root);
+            currentSize++;
+        }
+    }
 }
